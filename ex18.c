71fb6bace6aece2d293656683ae5b844f13dee24
Lawrence Velázquez <larryv@me.com>
2012-04-18 01:14:26 -0400

    Ex. 18: Add a heapsort implementation. [19.3.5]
    ex18.c
        - Add typedef for pointers to sorting functions.
        - Implement heapsort and ancillary functions.
        - Change test_sorting() to use arbitrary sorting functions.
        - Add tests for heapsort.

--- ex18.c
+++ ex18.c
@@ -19,6 +19,7 @@ void die(const char *message)
 // a typedef creates a fake type, in this
 // case for a function pointer
 typedef int (*compare_cb)(int a, int b);
+typedef int *(*sort_cb)(int *numbers, int count, compare_cb cmp);
 
 /*
  * A classic bubble sort function that uses the
@@ -48,6 +49,84 @@ int *bubble_sort(int *numbers, int count, compare_cb cmp)
     return target;
 }
 
+/* Heapsort implementation from CLRS, 3rd ed. */
+
+/* Heap primitives */
+#define PARENT(i) (((i) - 1) >> 1)
+#define LEFT(i) (((i) << 1) + 1)
+#define RIGHT(i) (((i) + 1) << 1)
+
+struct Heap {
+    int *array;
+    size_t len;
+    size_t heap_sz;
+};
+
+int max_heapify(struct Heap *A, size_t i, compare_cb cmp)
+{
+    size_t l, r, largest;
+    int tmp;
+
+    if (!A || !A->array) {
+        errno = EFAULT;
+        return 1;
+    }
+
+    l = LEFT(i);
+    r = RIGHT(i);
+    largest = (l < A->heap_sz && cmp(A->array[l], A->array[i]) > 0) ? l : i;
+    if (r < A->heap_sz && cmp(A->array[r], A->array[largest]) > 0)
+        largest = r;
+    if (largest != i) {
+        tmp = A->array[i];
+        A->array[i] = A->array[largest];
+        A->array[largest] = tmp;
+        max_heapify(A, largest, cmp);
+    }
+    return 0;
+}
+
+struct Heap *build_max_heap(int *input, size_t len, compare_cb cmp)
+{
+    struct Heap *A;
+    ssize_t i;
+
+    if (!input) {
+        errno = EFAULT;
+        return NULL;
+    }
+
+    if (!(A = malloc(sizeof(struct Heap))))
+        die("Memory error.");
+    A->array = input;
+    A->heap_sz = A->len = len;
+    for (i = A->len / 2 - 1; i >= 0; --i)
+        max_heapify(A, i, cmp);
+    return A;
+}
+
+/* Operates on a copy of the input array, not on the original. */
+int *heap_sort(int *numbers, int count, compare_cb cmp)
+{
+    int *target, tmp;
+    struct Heap *A;
+    size_t i;
+
+    if (!(target = malloc(count * sizeof(int))))
+        die("Memory error.");
+    memcpy(target, numbers, count * sizeof(int));
+
+    A = build_max_heap(target, count, cmp);
+    for (i = A->len - 1; i > 0; --i) {
+        tmp = A->array[0];
+        A->array[0] = A->array[i];
+        A->array[i] = tmp;
+        --A->heap_sz;
+        max_heapify(A, 0, cmp);
+    }
+    return target;
+}
+
 int sorted_order(int a, int b)
 {
     return a - b;
@@ -71,10 +150,10 @@ int strange_order(int a, int b)
  * Used to test that we are sorting things correctly
  * by doing the sort and printing it out.
  */
-void test_sorting(int *numbers, int count, compare_cb cmp)
+void test_sorting(int *numbers, int count, sort_cb sort, compare_cb cmp)
 {
     int i = 0;
-    int *sorted = bubble_sort(numbers, count, cmp);
+    int *sorted = sort(numbers, count, cmp);
 
     if (!sorted) die("Failed to sort as requested.");
 
@@ -102,9 +181,12 @@ int main(int argc, char **argv)
         numbers[i] = atoi(inputs[i]);
     }
 
-    test_sorting(numbers, count, sorted_order);
-    test_sorting(numbers, count, reverse_order);
-    test_sorting(numbers, count, strange_order);
+    test_sorting(numbers, count, bubble_sort, sorted_order);
+    test_sorting(numbers, count, bubble_sort, reverse_order);
+    test_sorting(numbers, count, bubble_sort, strange_order);
+    test_sorting(numbers, count, heap_sort, sorted_order);
+    test_sorting(numbers, count, heap_sort, reverse_order);
+    test_sorting(numbers, count, heap_sort, strange_order);
 
     free(numbers);
 
