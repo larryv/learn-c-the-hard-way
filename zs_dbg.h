e43e91a18927a3d778f28f6e47916cbe20fcf7d9
Lawrence Velázquez <larryv@me.com>
2012-04-20 05:14:21 -0400

    Add Zed's Awesome Debug Macros. [Ch. 21]
    zs_dbg.h
        - Add header file containing several useful debugging macros.
            - log_err(), log_warn(), log_info(), and debug() are for
              printing messages.
            - check(), sentinel(), check_mem(), and check_debug() are for
              testing conditions.

---
+++ zs_dbg.h
@@ -0,0 +1,67 @@
+#ifndef __ZS_DBG_H__
+#define __ZS_DBG_H__
+
+/* Zed Shaw's Awesome Debug Macros */
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#ifdef NDEBUG
+#define debug(M, ...)
+#else
+#define debug(M, ...) \
+    do { \
+        fprintf(stderr, "DEBUG %s:%d " M "\n", \
+                __FILE__, __LINE__, ##__VA_ARGS__); \
+    } while (0)
+#endif
+
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+
+#define log_err(M, ...) \
+    do { \
+        fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", \
+                __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__); \
+    } while (0)
+
+#define log_warn(M, ...) \
+    do { \
+        fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", \
+                __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__); \
+    } while (0)
+
+#define log_info(M, ...) \
+    do { \
+        fprintf(stderr, "[INFO] (%s:%d) " M "\n", \
+                __FILE__, __LINE__, ##__VA_ARGS__); \
+    } while (0)
+
+#define check(A, M, ...) \
+    do { \
+        if (!(A)) { \
+            log_err(M, ##__VA_ARGS__); \
+            errno = 0; \
+            goto error; \
+        } \
+    } while (0)
+
+#define sentinel(M, ...) \
+    do { \
+        log_err(M, ##__VA_ARGS__); \
+        errno = 0; \
+        goto error; \
+    } while (0)
+
+#define check_mem(A) check((A), "Out of memory.")
+
+#define check_debug(A, M, ...) \
+    do { \
+        if (!(A)) { \
+            debug(M, ##__VA_ARGS__); \
+            errno = 0; \
+            goto error; \
+        } \
+    } while (0)
+
+#endif /* end of include guard: __ZS_DBG_H__ */
