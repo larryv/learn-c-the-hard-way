c9202f38d9f55ca31a732481fa7967b6321b0248
Lawrence Velázquez <vq@larryv.me>
2016-02-14 21:04:52 -0500

    ex1: Add starter code (p. 6)
    Exercise 1 is a small demonstration of a variety of C features.

---
+++ ex1.c
@@ -0,0 +1,17 @@
+/*
+    Exercise 1: Dust Off That Compiler
+    A small demonstration of a variety of C features.
+*/
+
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+
+    return 0;
+}
