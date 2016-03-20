f3b607f9466a7eae776042ec6d04db1e40eadeb1
Lawrence Velázquez <vq@larryv.me>
2016-03-20 19:57:47 -0400

    ex8: Add starter code (p. 36)
    Exercise 8 is a silly program that prints out the command-line arguments
    or an insult, depending on the number of arguments provided.

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ SHELL := /bin/sh
 
 CFLAGS += -Wall -g
 
-exercises := 1 3 7
+exercises := 1 3 7 8
 exercises := $(exercises:%=ex%)
 
 .PHONY: all clean
---
+++ ex8.c
@@ -0,0 +1,26 @@
+/*
+    Exercise 8: If, Else-If, Else
+    Print out either args or insults, depending on number of args.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    if (argc == 1) {
+        printf("You only have one argument. You suck.\n");
+    } else if (argc > 1 && argc < 4) {
+        printf("Here's your arguments:\n");
+
+        for (i = 0; i < argc; i++) {
+            printf("%s ", argv[i]);
+        }
+        printf("\n");
+    } else {
+        printf("You have too many arguments. You suck.\n");
+    }
+
+    return 0;
+}
