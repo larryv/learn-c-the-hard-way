327bff8e37013a3a5252822040ff225a1c53edeb
Lawrence Velázquez <vq@larryv.me>
2016-03-20 23:25:13 -0400

    ex8: Consider user-provided args only (p. 38)
    The intent of the program is to count user-provided arguments, so
    exclude argv[0], which is (usually) the program name.

--- ex8.c
+++ ex8.c
@@ -9,12 +9,12 @@ int main(int argc, char *argv[])
 {
     int i = 0;
 
-    if (argc == 1) {
+    if (argc == 2) {
         printf("You only have one argument. You suck.\n");
-    } else if (argc > 1 && argc < 4) {
+    } else if (argc > 2 && argc < 5) {
         printf("Here's your arguments:\n");
 
-        for (i = 0; i < argc; i++) {
+        for (i = 1; i < argc; i++) {
             printf("%s ", argv[i]);
         }
         printf("\n");
