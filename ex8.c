89b6c79e53d46dd6d952ce44f5a20bd8e426dbc3
Lawrence Velázquez <vq@larryv.me>
2016-03-21 11:51:17 -0400

    ex8: Don't print trailing space after arg list

--- ex8.c
+++ ex8.c
@@ -14,8 +14,9 @@ int main(int argc, char *argv[])
     } else if (argc > 2 && argc < 5) {
         printf("Here's your arguments:\n");
 
-        for (i = 1; i < argc; i++) {
-            printf("%s ", argv[i]);
+        printf("%s", argv[1]);
+        for (i = 2; i < argc; i++) {
+            printf(" %s", argv[i]);
         }
         printf("\n");
     } else {
