74ab2b9a7cc8fc8d7ee24b335f06b4ddc29f43b7
Lawrence Velázquez <vq@larryv.me>
2016-04-10 18:14:21 -0400

    ex12: Treat results of sizeof as size_t values
    The type "size_t" is the platform-independent return type of the sizeof
    operator (C11 §7.19 ¶2), so the most portable printf specifier is "zu"
    (C11 §7.21.6.1 ¶7).

--- ex12.c
+++ ex12.c
@@ -15,20 +15,18 @@ int main(int argc, char *argv[])
         'S', 'h', 'a', 'w', '\0'
     };
 
-    // WARNING: On some systems you may have to change the
-    // %ld in this code to a %u since it will use unsigned ints
-    printf("The size of an int: %ld\n", sizeof(int));
-    printf("The size of areas (int[]): %ld\n", sizeof(areas));
-    printf("The number of ints in areas: %ld\n",
+    printf("The size of an int: %zu\n", sizeof(int));
+    printf("The size of areas (int[]): %zu\n", sizeof(areas));
+    printf("The number of ints in areas: %zu\n",
             sizeof(areas) / sizeof(int));
     printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
 
-    printf("The size of a char: %ld\n", sizeof(char));
-    printf("The size of name (char[]): %ld\n", sizeof(name));
-    printf("The number of chars: %ld\n", sizeof(name) / sizeof(char));
+    printf("The size of a char: %zu\n", sizeof(char));
+    printf("The size of name (char[]): %zu\n", sizeof(name));
+    printf("The number of chars: %zu\n", sizeof(name) / sizeof(char));
 
-    printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
-    printf("The number of chars: %ld\n",
+    printf("The size of full_name (char[]): %zu\n", sizeof(full_name));
+    printf("The number of chars: %zu\n",
             sizeof(full_name) / sizeof(char));
 
     printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
