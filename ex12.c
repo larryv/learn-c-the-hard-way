0643c34cdbb82a53320c821a77da32a0a45cc44f
Lawrence Velázquez <vq@larryv.me>
2016-04-10 23:30:30 -0400

    ex12: Tweak output
    - Print "byte" or "bytes" after sizes. (Cheating: Macros, ternary
      operators.)
    
    - Print numbers of array elements on same lines as array sizes.
    
    - Throw in some quotation marks.
    
    - Reword and rearrange a bit.

--- ex12.c
+++ ex12.c
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 
+#define BYTE_OR_BYTES(n) (sizeof(n) == 1 ? "byte" : "bytes")
+
 int main(int argc, char *argv[])
 {
     int areas[] = { 10, 12, 13, 14, 20 };
@@ -15,21 +17,32 @@ int main(int argc, char *argv[])
         'S', 'h', 'a', 'w', '\0'
     };
 
-    printf("The size of an int: %zu\n", sizeof(int));
-    printf("The size of areas (int[]): %zu\n", sizeof(areas));
-    printf("The number of ints in areas: %zu\n",
-            sizeof(areas) / sizeof(int));
-    printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
+    printf("The size of an int: %zu %s\n",
+            sizeof(int), BYTE_OR_BYTES(int));
+    printf("The size of \"areas\" (int[%zu]): %zu %s\n",
+            sizeof(areas) / sizeof(int),
+            sizeof(areas),
+            BYTE_OR_BYTES(areas));
+    printf("The first area is %d; the second is %d.\n",
+            areas[0],
+            areas[1]);
 
-    printf("The size of a char: %zu\n", sizeof(char));
-    printf("The size of name (char[]): %zu\n", sizeof(name));
-    printf("The number of chars: %zu\n", sizeof(name) / sizeof(char));
+    printf("The size of a char: %zu %s\n",
+            sizeof(char),
+            BYTE_OR_BYTES(char));
+    printf("The size of \"name\" (char[%zu]): %zu %s\n",
+            sizeof(name) / sizeof(char),
+            sizeof(name),
+            BYTE_OR_BYTES(name));
+    printf("\"name\" is \"%s\"\n",
+            name);
 
-    printf("The size of full_name (char[]): %zu\n", sizeof(full_name));
-    printf("The number of chars: %zu\n",
-            sizeof(full_name) / sizeof(char));
-
-    printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
+    printf("The size of \"full_name\" (char[%zu]): %zu %s\n",
+            sizeof(full_name) / sizeof(char),
+            sizeof(full_name),
+            BYTE_OR_BYTES(full_name));
+    printf("\"full_name\" is \"%s\"\n",
+            full_name);
 
     return 0;
 }
