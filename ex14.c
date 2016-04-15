9f04d84c5c458ba7bd57dcc288bc9216e0844ef8
Lawrence Velázquez <vq@larryv.me>
2016-04-15 16:12:31 -0400

    ex14: Rework functions (p. 60)
    - Inline can_print_it.
    
    - Swap print_arguments and print_letters to avoid forward declarations.

--- ex14.c
+++ ex14.c
@@ -6,9 +6,20 @@
 #include <stdio.h>
 #include <ctype.h>
 
-// forward declarations
-int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char arg[])
+{
+    int i = 0;
+
+    for (i = 0; arg[i] != '\0'; i++) {
+        char ch = arg[i];
+
+        if (isalpha(ch) || isblank(ch)) {
+            printf("'%c' == %d ", ch, ch);
+        }
+    }
+
+    printf("\n");
+}
 
 void print_arguments(int argc, char *argv[])
 {
@@ -17,26 +28,6 @@ void print_arguments(int argc, char *argv[])
     for (i = 0; i < argc; i++) {
         print_letters(argv[i]);
     }
-}
-
-void print_letters(char arg[])
-{
-    int i = 0;
-
-    for (i = 0; arg[i] != '\0'; i++) {
-        char ch = arg[i];
-
-        if (can_print_it(ch)) {
-            printf("'%c' == %d ", ch, ch);
-        }
-    }
-
-    printf("\n");
-}
-
-int can_print_it(char ch)
-{
-    return isalpha(ch) || isblank(ch);
 }
 
 int main(int argc, char *argv[])
