fcf804c70e76fdd1d96ede4ad5b1b53ee468d370
Lawrence Velázquez <vq@larryv.me>
2016-04-16 12:40:56 -0400

    ex15: Print the arrays backwards too (p. 67)

--- ex15.c
+++ ex15.c
@@ -19,34 +19,50 @@ int main(int argc, char *argv[])
     int i = 0;
 
     // first way using indexing
+    printf("--- Index notation with arrays (forwards) ---\n");
     for (i = 0; i < count; i++) {
         printf("%s has %d years alive.\n", names[i], ages[i]);
     }
-
-    printf("---\n");
+    printf("\n--- Index notation with arrays (backwards) ---\n");
+    for (i = count - 1; i >= 0; i--) {
+        printf("%s has %d years alive.\n", names[i], ages[i]);
+    }
 
     // set up the pointers to the start of the arrays
     int *cur_age = ages;
     char **cur_name = names;
 
     // second way using pointers
+    printf("\n--- Offsets with pointer arithmetic (forwards) ---\n");
     for (i = 0; i < count; i++) {
         printf("%s is %d years old.\n",
                 *(cur_name + i), *(cur_age + i));
     }
-
-    printf("---\n");
+    printf("\n--- Offsets with pointer arithmetic (backwards) ---\n");
+    for (i = count - 1; i >= 0; i--) {
+        printf("%s is %d years old.\n",
+                *(cur_name + i), *(cur_age + i));
+    }
 
     // third way, pointers are just arrays
+    printf("\n--- Index notation with pointers (forwards) ---\n");
     for (i = 0; i < count; i++) {
         printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
     }
-
-    printf("---\n");
+    printf("\n--- Index notation with pointers (backwards) ---\n");
+    for (i = count - 1; i >= 0; i--) {
+        printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
+    }
 
     // fourth way with pointers in a stupid complex way
+    printf("\n--- Walking with pointer arithmetic (forwards) ---\n");
     for (cur_name = names, cur_age = ages;
             (cur_age - ages) < count; cur_name++, cur_age++) {
+        printf("%s lived %d years so far.\n", *cur_name, *cur_age);
+    }
+    printf("\n--- Walking with pointer arithmetic (backwards) ---\n");
+    for (cur_name = names + count - 1, cur_age = ages + count - 1;
+            cur_age >= ages; cur_name--, cur_age--) {
         printf("%s lived %d years so far.\n", *cur_name, *cur_age);
     }
 
