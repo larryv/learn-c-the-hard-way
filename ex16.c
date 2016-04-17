56c030cc79ea9670d69f13ff2d7b546b2b5a0a59
Lawrence Velázquez <vq@larryv.me>
2016-04-17 17:47:01 -0400

    ex16: Avoid pointers and heap allocation (p. 72)
    Cheated a little by setting up the structs using initializer lists
    instead of assigning to the members individually.

--- ex16.c
+++ ex16.c
@@ -45,21 +45,42 @@ void Person_print(struct Person *who)
     printf("\tWeight: %d\n", who->weight);
 }
 
+void Person_print_noptr(struct Person who)
+{
+    printf("Name: %s\n", who.name);
+    printf("\tAge: %d\n", who.age);
+    printf("\tHeight: %d\n", who.height);
+    printf("\tWeight: %d\n", who.weight);
+}
+
 int main(int argc, char *argv[])
 {
     // make two people structures
+    /*
     struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
-
     struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
+    */
+
+    struct Person joe = { "Joe Alex", 32, 64, 140 };
+    struct Person frank = { "Frank Blank", 20, 72, 180 };
 
     // print them out and where they are in memory
+    /*
     printf("Joe is at memory location %p:\n", joe);
     Person_print(joe);
 
     printf("Frank is at memory location %p:\n", frank);
     Person_print(frank);
+    */
+
+    printf("Joe is at memory location %p:\n", &joe);
+    Person_print_noptr(joe);
+
+    printf("Frank is at memory location %p:\n", &frank);
+    Person_print_noptr(frank);
 
     // make everyone age 20 years and print them again
+    /*
     joe->age += 20;
     joe->height -= 2;
     joe->weight += 40;
@@ -68,10 +89,22 @@ int main(int argc, char *argv[])
     frank->age += 20;
     frank->weight += 20;
     Person_print(frank);
+    */
+
+    joe.age += 20;
+    joe.height -= 2;
+    joe.weight += 40;
+    Person_print_noptr(joe);
+
+    frank.age += 20;
+    frank.weight += 20;
+    Person_print_noptr(frank);
 
     // destroy them both so we clean up
+    /*
     Person_destroy(joe);
     Person_destroy(frank);
+    */
 
     return 0;
 }
